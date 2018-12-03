5fdaa131a3cf8842affbe0ef5cb3870c7747bb9f
vincentwu08 <vincentwu08@gmail.com>
2018-12-03 20:02:20 +0800

    v-bind

--- Demo1/index.js
+++ Demo1/index.js
@@ -1,6 +1,15 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      timeMessage: '页面加载于 ' + new Date().toLocaleString()
     }
+})
+
+var app1 = new Vue({
+  el: '#app1',
+  data: {
+    message: 'Hello Vue1!',
+    timeMessage: '1页面加载于 ' + new Date().toLocaleString()
+  }
 })
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # VuePractice
 Vue练习
+
+##Demo1
+Hello Vue
+一般来说只能有一个挂载点，在使用脚手架工具vue-cli中就会严格控制你，这两个div都要控制在一个大的div中
+
+v-bind:title="timeMessage"
+将这个元素节点的 title 特性和 Vue 实例的 timeMessage 属性保持一致
+
